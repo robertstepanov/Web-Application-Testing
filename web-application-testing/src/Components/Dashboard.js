793e43e4f72b5b4d5849fbe2b3a1bd7a956e2551
Robert Stepanov <robertdstepanov@gmail.com>
2020-01-16 21:31:32 -0500

    Hmm

--- web-application-testing/src/Components/Dashboard.js
+++ web-application-testing/src/Components/Dashboard.js
@@ -3,47 +3,14 @@ import React from "react";
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { strikes: 0, balls: 0, fouls: "0", hits: "0" };
+    this.state = { strikes: 0, balls: 0, fouls: 0, hits: 0 };
   }
 
   addStrike = this.addStrike.bind(this);
   addBall = this.addBall.bind(this);
-  // const [balls, setBalls] = useState(0);
-  // const [strikes, setStrikes] = useState(0);
-  // const [foul] = useState(0);
-  // const [hit] = 0;
-
-  // const addBall = () => {
-  //   if (balls > 3) {
-  //     setBalls(0);
-  //     setStrikes(0);
-  //   } else {
-  //     setBalls(balls + 1);
-  //   }
-  // };
-  // const addStrike = () => {
-  //   if (strikes > 2) {
-  //     setStrikes(0);
-  //     setBalls(0);
-  //   } else {
-  //     setStrikes(strikes + 1);
-  //   }
-  // };
-  // const addFoul = () => {
-  //   if (foul) {
-  //     if (setStrikes === 2) {
-  //       setStrikes(strikes);
-  //     } else {
-  //       setStrikes(strikes + 1);
-  //     }
-  //   }
-  // };
-  // const addHit = () => {
-  //   if (hit) {
-  //     return;
-  //     setStrikes(0);
-  //     setBalls(0);
-  //   }
+  addFouls = this.addFouls.bind(this);
+  addHits = this.addHits.bind(this);
+  
 
   addStrike() {
     this.setState({
@@ -55,6 +22,17 @@ class Dashboard extends React.Component {
       balls: this.state.balls + 1
     });
   }
+  addFouls() {
+    this.setState({
+      fouls: this.state.fouls + 1
+    });
+  }
+  addHits() {
+    this.setState({
+      hits: this.state.hits + 1
+    });
+  }
+
   render() {
     return (
       <div className="dash-wrapper">
@@ -68,11 +46,11 @@ class Dashboard extends React.Component {
         </div>
         <div>
           <h1>{this.state.fouls}</h1>
-          <button onClick={this.fouls}>Add Foul</button>
+          <button onClick={this.addFouls}>Add Foul</button>
         </div>
         <div>
           <h1>{this.state.hits}</h1>
-          <button onClick={this.hits}>Add Hit</button>
+          <button onClick={this.addHits}>Add Hit</button>
         </div>
       </div>
     );
--- web-application-testing/src/Components/Display.js
+++ web-application-testing/src/Components/Display.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-
-const BallsStrikes = props => {
+const Display = props => {
   return (
     <div className="count-wrapper">
       <div className="strikes-container">
         <h2>Strikes</h2>
-        <div className="strikes-number">{props.strikes}</div>
+        <div className="strikes-number">{props.addStrike}</div>
       </div>
       <div className="balls-container">
         <h2>Balls</h2>
-        <div className="balls-number">{props.balls}</div>
+        <div className="balls-number">{props.addBall}</div>
       </div>
     </div>
   );
 };
 
-export default BallsStrikes;
+export default Display;
